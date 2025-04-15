e8597fc8d28cdd2c05f23d75505a1acd02c66889
= <sebastiangiulianelli02@gmail.com>
2025-04-15 17:59:38 -0300

    agrego redblack

---
+++ navierstokes-redblack/solver.h
@@ -0,0 +1,11 @@
+//
+// solver.h
+//
+
+#ifndef SOLVER_H_INCLUDED
+#define SOLVER_H_INCLUDED
+
+void dens_step(unsigned int n, float *x, float *x0, float *u, float *v, float diff, float dt);
+void vel_step(unsigned int n, float *u, float *v, float *u0, float *v0, float visc, float dt);
+
+#endif /* SOLVER_H_INCLUDED */
